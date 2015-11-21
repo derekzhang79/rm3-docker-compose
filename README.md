2be05538a7b59f46985aff2e856cde86189e6a79
Ken Wronkiewicz <wh-github@wirewd.com>
2015-11-21 09:15:52 -0800

    initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+docker-compose build
+docker-compose run web bash
+docker-compose up -d
---
+++ web/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+createdb rm3test -U wirehead -h postgres
+psql rm3test -U wirehead -h postgres < db-schema.sql
+export RM3_PG=postgresql://wirehead:rm3test@postgres/rm3test 
+./bin/rm3load -f default_frontpage.json
+./bin/rm3load -f default_users.json
+./bin/rm3admin adduser wirehead "Ken Wronkiewicz" -p "Some profile text" -u http://www.wirewd.com/ -e wh@wirewd.com --password password
+./bin/rm3admin assign wirehead root
+./bin/rm3admin permit root edit \*
+./bin/rm3admin permit root delete \*
+./bin/rm3admin permit root view \*
+./bin/rm3admin permit root grant \*
+./bin/rm3admin permit root viewdraft \*
+./bin/rm3admin permit nobody view wh.!users

8ca894283100138fcdf221bae70b184fea868834
Ken Wronkiewicz <wh-github@wirewd.com>
2015-11-21 14:43:36 -0800

    tweaked config slightly, removed need for password dialogs.

--- web/setup.sh
+++ web/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 createdb rm3test -U wirehead -h postgres
 psql rm3test -U wirehead -h postgres < db-schema.sql
-export RM3_PG=postgresql://wirehead:rm3test@postgres/rm3test 
 ./bin/rm3load -f default_frontpage.json
 ./bin/rm3load -f default_users.json
 ./bin/rm3admin adduser wirehead "Ken Wronkiewicz" -p "Some profile text" -u http://www.wirewd.com/ -e wh@wirewd.com --password password
